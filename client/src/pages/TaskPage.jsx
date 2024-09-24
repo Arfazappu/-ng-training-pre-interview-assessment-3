9cb9fa821858119bd068630575aebe5962c8fb91
Arfaz Ahmed <arfazaar14@gmail.com>
2024-09-24 13:42:38 +0530

    intial setup

---
+++ client/src/pages/TaskPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopBar from '../components/uiComponents/topBar/TopBar'
+
+function TaskPage() {
+  return (
+    <div>
+        <TopBar/>
+    </div>
+  )
+}
+
+export default TaskPage
