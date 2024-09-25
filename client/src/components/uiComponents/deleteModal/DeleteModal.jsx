81f99cc9dcb52dbf25cc60cd94f562ce473f2ce4
Arfaz Ahmed <arfazaar14@gmail.com>
2024-09-25 16:20:47 +0530

    resolved delete modal issue

--- client/src/components/uiComponents/deleteModal/DeleteModal.jsx
+++ client/src/components/uiComponents/deleteModal/DeleteModal.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../button/Button';
+import { RxCross2 } from 'react-icons/rx';
 
 function DeleteModal({ isOpen, onClose, onDelete, task }) {
   if (!isOpen) return null;
@@ -10,16 +12,18 @@ function DeleteModal({ isOpen, onClose, onDelete, task }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg p-6">
-        <h2 className="text-xl font-semibold mb-4">Confirm Deletion</h2>
-        <p>Are you sure you want to delete the task: <strong>{task.assignedTo}</strong>?</p>
-        <div className="flex justify-end mt-4">
-          <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded mr-2" onClick={onClose}>
-            Cancel
-          </button>
-          <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={handleDelete}>
-            Delete
-          </button>
+      <div className="bg-white rounded-lg shadow-lg relative overflow-hidden">
+      <div 
+        className="absolute top-0 right-0 cursor-pointer p-1" 
+        onClick={onClose} 
+      >
+        <RxCross2 color='white' size={20}/>
+      </div>
+        <h2 className="bg-red-500 text-white p-4 text-xl text-center font-semibold mb-4">Delete</h2>
+        <p className='px-4 py-2'>Are you sure you want to delete the task: <strong>{task.assignedTo}</strong>?</p>
+        <div className="flex justify-end mt-4 px-4 py-2">
+          <Button btnText='No' variant='primary' onClick={onClose} customClass='mx-1'/>
+          <Button btnText='Yes' onClick={handleDelete}/>
         </div>
       </div>
     </div>
